182bcebe288159687d17c9595fd806aed2d03d54
Luis Felipe <39298035+lfmramos@users.noreply.github.com>
2024-01-31 17:02:27 +0000

    Delete Semana 2 directory

--- Semana 2/dezenas.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Digite um número inteiro:"))
-
-calculo = numero // 10
-dezena = calculo % 10
-
-print("O dígito das dezenas é", dezena)
--- Semana 2/educado.py
+++
@@ -1,4 +0,0 @@
-nameMother = input("Qual o nome da sua mãe? ")
-nameFather = input("Qual o nome do seu pai? ")
-
-print("Bom dia Sra.", nameMother,"!!! E bom dia Sr.", nameFather,".")
