74bbbce8f2b74f2fe50efc6d213267a1e69239c6
Luis Felipe <lfelipe.sm@gmail.com>
2024-02-15 18:27:38 +0000

    performs a sequential search

---
+++ Parte 2/Semana 4/busca_sequencial.py
@@ -0,0 +1,8 @@
+# Busca sequencial
+
+def busca_sequencial(seq, x):
+    '''(list, float) -> bool'''
+    for i in range(len(seq)):
+        if seq[i] == x:
+            return True
+    return False
