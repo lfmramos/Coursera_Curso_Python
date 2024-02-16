7ae3a4b5c204e1b66a7aa7e58c3981bcf1dfee7b
Luis Felipe <lfelipe.sm@gmail.com>
2024-02-16 18:25:59 +0000

    Update bubblesort.py

--- Parte 2/Semana 5/bubblesort.py
+++ Parte 2/Semana 5/bubblesort.py
@@ -17,6 +17,17 @@ class Ordenador:
             for j in range(i):
                 if lista[j] > lista[j + 1]:
                     lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    def bolha_curta(self, lista):
+        fim = len(lista)
+        for i in range(fim - 1, 0, -1):
+            trocou = False
+            for j in range(i):
+                if lista[j] > lista[j + 1]:
+                    lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                    trocou = True
+            if not trocou:
+                return
 # Para testar o programa:
 # 1) Criar uma lista com valores desordenados, i.e, lista = [x, y, z]
 # 2) Instanciar um objeto da classe Ordenador, i.e, o = Ordenador()
